ec3690cb9f6ba0ceba0b585afcbae1bc20ccaa25
valeryvkrukov <valery.v.krukov@gmail.com>
2022-04-09 00:14:23 +0300

    Stubs CRUD + listing with pagination

--- src/Controller/Therapy/StubController.php
+++ src/Controller/Therapy/StubController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Therapy;
 
+
 use App\Entity\Therapy\Stub;
 use App\Form\Therapy\StubType;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,15 +17,50 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class StubController extends AbstractController
 {
+    const PAGINATION_PAGE = 5;
     protected EntityManagerInterface $entityManager;
     protected TranslatorInterface $translator;
+    protected PaginatorInterface $paginator;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        TranslatorInterface $translator
+        TranslatorInterface $translator,
+        PaginatorInterface $paginator
     ) {
         $this->entityManager = $entityManager;
         $this->translator = $translator;
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     */
+    #[Route('/{_locale<%app.supported_locales%>}/therapy/stubs', name: 'app_therapy_stubs_list')]
+    public function index(Request $request): Response
+    {
+        $repository = $this->entityManager->getRepository(Stub::class);
+        $total = $repository
+            ->createQueryBuilder('total')
+            ->select('COUNT(total.id)')
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+        $query = $repository
+            ->createQueryBuilder('stub')
+            ->setFirstResult(($request->query->getInt('page')))
+            ->setMaxResults(self::PAGINATION_PAGE)
+            ->getQuery()
+        ;
+        $pagination = $this->paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            6
+        );
+
+        return $this->render('therapy/stub/list.html.twig', [
+            'pagination' => $pagination,
+        ]);
     }
 
     #[Route('/{_locale<%app.supported_locales%>}/therapy/stub/new', name: 'app_therapy_stub_new')]
@@ -31,7 +70,13 @@ class StubController extends AbstractController
         $stubForm->handleRequest($request);
 
         if ($stubForm->isSubmitted() && $stubForm->isValid()) {
-            // TODO
+            $data = $stubForm->getData();
+
+            $stub = $this->entityManager
+                ->getRepository(Stub::class)
+                ->getNewStubObjectFromArray($data);
+
+            return $this->redirectToRoute('app_therapy_stub_edit', ['id' => $stub->getId()]);
         }
 
         return $this->render('therapy/stub/index.html.twig', [
@@ -43,17 +88,20 @@ class StubController extends AbstractController
     #[Route('/{_locale<%app.supported_locales%>}/therapy/stub/edit/{id<\d+>}', name: 'app_therapy_stub_edit')]
     public function editStub(Request $request, int $id): Response
     {
-        $stub = $this->entityManager->getRepository(Stub::class)->find($id);
+        $repository = $this->entityManager->getRepository(Stub::class);
+        $stub = $repository->find($id);
 
         if (!$stub) {
             // TODO exception about stub not exists
         }
 
-        $stubForm = $this->createForm(StubType::class, $stub);
+        $stubForm = $this->createForm(StubType::class, $repository->getStubObjectFromEntity($stub));
         $stubForm->handleRequest($request);
 
         if ($stubForm->isSubmitted() && $stubForm->isValid()) {
-            // TODO
+            $data = $stubForm->getData();
+
+            $repository->updateEntityFromDto($stub, $data);
         }
 
         return $this->render('therapy/stub/index.html.twig', [
---
+++ src/Entity/DTO/StubObject.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity\DTO;
+
+
+class StubObject
+{
+    public int $id;
+    public string $name;
+    public string $description;
+    public string $background;
+    public array $labels = [];
+}
