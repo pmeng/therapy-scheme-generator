eb06c99388a0ece2b9703684b6433ecfaa0b61f7
valeryvkrukov <valery.v.krukov@gmail.com>
2022-04-09 17:12:16 +0300

    Added PDF reports & UI fixes

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
