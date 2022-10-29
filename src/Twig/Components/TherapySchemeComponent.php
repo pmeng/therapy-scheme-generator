721d7d37d853c224af677d1f55d16ee1807bce45
Valery Krukov <valery.v.krukov@gmail.com>
2022-10-29 17:47:25 +0300

    Basic "hierarchical" labels search implementation

---
+++ src/Twig/Components/TherapySchemeComponent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Repository\Therapy\LabelRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+
+#[AsLiveComponent('therapy-scheme')]
+final class TherapySchemeComponent
+{
+    use DefaultActionTrait;
+
+    private LabelRepository $labelRepository;
+
+    #[LiveProp(writable: true)]
+    public ?string $labelsRequest = null;
+    #[LiveProp(writable: true)]
+    public ?bool $suppressLabels = null;
+    #[LiveProp(writable: true)]
+    public ?bool $useExcerpt = null;
+
+    public function __construct(LabelRepository $labelRepository)
+    {
+        $this->labelRepository = $labelRepository;
+    }
+
+    public function getLabels(): array
+    {
+        if ($this->labelsRequest === null) {
+            return [];
+        } else {
+            return $this->labelRepository->findLabelsByRequest($this->labelsRequest);
+        }
+    }
+
+    public function getSuppressLabels(): bool
+    {
+        return $this->suppressLabels ? true : false;
+    }
+
+    public function getUseExcerpt(): bool
+    {
+        return $this->useExcerpt ? true : false;
+    }
+
+    public function getLabelsRequest(): string
+    {
+        return $this->labelsRequest ?? '';
+    }
+}
