b6ded2a7e356c071a8547074af0e3f149a0712e1
Dhia Djobbi <dhia.djobbi@avaxia-group.com>
2023-03-14 23:37:15 +0100

    fix/issue-25 - Generated a new migration

---
+++ migrations/Version20230314223647.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230314223647 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stub CHANGE description description LONGTEXT NOT NULL, CHANGE excerpt excerpt LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stub CHANGE description description VARCHAR(500) NOT NULL, CHANGE excerpt excerpt VARCHAR(255) DEFAULT NULL');
+    }
+}
