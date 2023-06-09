f3b11324f1bd9f3c8a5b94fb74bb96acc8ba9bc0
Dhia Djobbi <dhia.djobbi@gmail.com>
2023-06-09 08:39:27 +0100

    fix/issue-33 - Create Label in Therapy Stub page

--- src/Form/Therapy/StubType.php
+++ src/Form/Therapy/StubType.php
@@ -4,16 +4,29 @@ namespace App\Form\Therapy;
 
 
 use App\Entity\Therapy\Label;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormEvents;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Repository\Therapy\LabelRepository;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class StubType extends AbstractType
 {
+    private LabelRepository $labelRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(LabelRepository $labelRepository, EntityManagerInterface $entityManager)
+    {
+        $this->labelRepository = $labelRepository;
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -52,6 +65,27 @@ class StubType extends AbstractType
                 'mapped' => false,
                 'attr' => ['style' => 'display: none;'],
             ]);
+
+            $builder->addEventListener(FormEvents::PRE_SUBMIT, function(PreSubmitEvent $event){
+                $data = $event->getData();
+                $labels = $data['labels'];
+
+                for ($i = 0; $i < count($labels); $i++) {
+                    if (!is_numeric($labels[$i])) {
+                        $label = new Label();
+                        $label->setShortName($labels[$i]);
+                        $label->setReportName($labels[$i]);
+                        $this->entityManager->persist($label);
+                        $this->entityManager->flush();
+
+                        $createdLabel = $this->labelRepository->findOneBy(['shortName' => $labels[$i]]);
+                        $labels[$i] = $createdLabel->getId();
+                    }
+                }
+
+                $data['labels'] = $labels;
+                $event->setData($data);
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
