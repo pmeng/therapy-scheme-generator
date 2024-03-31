d40fdd7ff2a48457282a4f817d852c54a3d231c3
Mahmoud Naoum <mahmoud.naoum@meng-tech.com>
2024-03-31 23:39:24 +0200

    adding category deletion warning when it is associated to a stub

--- src/Controller/Therapy/StubCategoryController.php
+++ src/Controller/Therapy/StubCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Therapy;
 
+use App\Entity\Therapy\Stub;
 use App\Entity\Therapy\StubCategory;
 use App\Form\Therapy\StubCategoryType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -44,6 +45,14 @@ class StubCategoryController extends AbstractController
             $request->query->getInt('page', 1),
             self::PAGINATION_PAGE
         );
+
+        $categories = $repository
+            ->createQueryBuilder('stub_category')
+            ->select('stub_category.id', 'stub_category.name')
+            ->orderBy('stub_category.name', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
     
         $stubCategoryForm = $this->createForm(StubCategoryType::class);
         $stubCategoryForm->handleRequest($request);
@@ -61,7 +70,7 @@ class StubCategoryController extends AbstractController
             'controller_name' => 'StubCategoryController',
             'pagination' => $pagination,
             'stubCategoryForm' => $stubCategoryForm->createView(),
-
+            'categories' => $categories,
         ]);
     }
 
@@ -122,6 +131,82 @@ class StubCategoryController extends AbstractController
         return new JsonResponse([
             'success' => 0,
             'message' => 'Stub Category not exists'
+        ]);
+    }
+    
+    #[Route('/{_locale<%app.supported_locales%>}/therapy/stub/category/check-usage/{id}', name: 'app_check_category_usage')]
+    public function checkStubUsage(int $id): Response
+    {
+        $repository = $this->entityManager->getRepository(Stub::class);
+
+        // Check if there are any stubs that use the given category ID
+        $stubs = $repository->createQueryBuilder('s')
+            ->select('s.id, s.name')
+            ->where('s.category = :categoryId')
+            ->setParameter('categoryId', $id)
+            ->getQuery()
+            ->getResult();
+    
+        $usedStubs = [];
+        foreach ($stubs as $stub) {
+            $usedStubs[] = [
+                'id' => $stub['id'],
+                'name' => $stub['name']
+            ];
+        }
+    
+        $isUsed = count($usedStubs) > 0;
+    
+        return new JsonResponse([
+            'isUsed' => $isUsed,
+            'usedStubs' => $usedStubs
+        ]);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/therapy/stub/category/replace/{id}', name: 'app_therapy_stub_category_replace')]
+    public function reassignStubs(Request $request, int $id): Response
+    {
+        $newCategoryId = $request->request->get('newCategoryId');
+        $newStubCategory = $this->entityManager->getRepository(StubCategory::class)->find($newCategoryId);
+
+        if (!$newCategoryId) {
+            return new JsonResponse([
+                'success' => 0,
+                'message' => 'New category ID is required'
+            ]);
+        }
+    
+        $repository = $this->entityManager->getRepository(Stub::class);
+        $stubs = $repository->createQueryBuilder('s')
+            ->where('s.category = :categoryId')
+            ->setParameter('categoryId', $id)
+            ->getQuery()
+            ->getResult();
+        
+        if (empty($stubs)) {
+            return new JsonResponse([
+                'success' => 0,
+                'message' => 'No stubs found for the given category ID'
+            ]);
+        }
+    
+        foreach ($stubs as $stub) {
+            $stub->setCategory( $newStubCategory );
+            $this->entityManager->persist($stub);
+        }
+        // Remove the stub category after reassignment
+        $stubCategory = $this->entityManager->getRepository(StubCategory::class)->find($id);
+    
+        $this->entityManager->flush();
+    
+    
+        if ($stubCategory) {
+            $this->entityManager->remove($stubCategory);
+            $this->entityManager->flush();
+        }
+    
+        return new JsonResponse([
+            'success' => 1
         ]);
     }
     
